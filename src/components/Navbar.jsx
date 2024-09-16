ae26c0eb27c98635826e00217fde9a27d5369434
rimfat1311 <simontimnan@gmail.com>
2024-09-16 06:22:10 -0700

    updete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,11 +5,11 @@ const Navbar = () => {
     <div className='flex w-full bg-white h-24 px-16 '>
       <ul className='flex flex-row w-full justify-between items-center '>
         <li>toggle</li>
-        <li className='text-3xl'>Dashboard</li>
+        <li className='text-3xl '>Dashboard</li>
         <li>Test User</li>
       </ul>
     </div>
   )
 }
 
-export default Navbar
+export default Navbar 
